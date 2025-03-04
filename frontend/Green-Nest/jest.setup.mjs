06dd456e4ef67de17abca79c249a6b2bec607e49
Nabia-Zeenat-Zia <nabiazeenatzia@gmail.com>
2025-03-04 12:54:58 +0000

    Fix: March 4

--- frontend/Green-Nest/jest.setup.mjs
+++ frontend/Green-Nest/jest.setup.mjs
@@ -1,3 +1,10 @@
 // jest.setup.js (ESM syntax)
 //globalThis.importMetaUrl = import.meta.url || 'file://';
+export default {
+  preset: 'react', // This preset works with React projects
+  testEnvironment: 'jsdom', // Set the right environment for frontend testing
+  transform: {
+    '^.+\\.[t|j]sx?$': 'babel-jest', // Use Babel to transpile JS/TS files
+  },
+};
 
